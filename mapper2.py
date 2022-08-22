fbae18132671a0136ef215ff71ced1145b606c3d
harithagoli <88888888+harithagoli@users.noreply.github.com>
2022-08-22 15:05:43 -0500

    Add files via upload

---
+++ mapper2.py
@@ -0,0 +1,6 @@
+"""mapper.py"""                                                                                                                                                                                                    
+import  sys                                                                                                                                                                                                        
+for line_raw in sys.stdin:                                                                                                                                                                                         
+    line =line_raw.strip().split(',')                                                                                                                                                                              
+    #print out: vin, inc_type, make, year                                                                                                                                                                          
+    print('%s-%s,1'%(line[1],line[2]))  
